8502614a3752fc0288e23877341ee59aca4cace2
Ansar <ansarrakhimov91@gmail.com>
2023-12-01 22:42:19 +0300

    Added list of commands and implemented Help,Author,Error respond

--- main.go
+++ main.go
@@ -47,12 +47,16 @@ func getUpdates(BotURL string, offset int) ([]types.Update, error) {
 }
 
 func respond(botURL string, update types.Update) error {
-	var botMessage types.BotMessage
+	botMessage := new(types.BotMessage)
 	botMessage.ChatId = update.Message.Chat.ChatId
 	if update.Message.Text == "/help" {
-		botMessage.Text = "Use that commands"
+		HelpRespond(botMessage)
+	} else if update.Message.Text == "/author" {
+		AuthorRespond(botMessage)
+	} else if update.Message.Text == "/info" {
+		InfoRespond(botMessage)
 	} else {
-		botMessage.Text = "Smth went wrong"
+		ErrorRespond(botMessage)
 	}
 	buf, err := json.Marshal(botMessage)
 	if err != nil {
@@ -63,4 +67,32 @@ func respond(botURL string, update types.Update) error {
 		return err
 	}
 	return nil
+}
+
+func HelpRespond(botMessage *types.BotMessage) {
+	botMessage.Text = "Hello, this bot can sort your doings and remind about them\n" +
+		"You can use this following commands\n" +
+		"/info - gets information about sorting methods\n" + //implemented
+		"/sort - sorts your doings, example:\n" +
+		"	/sort\n" +
+		"	Make smth 01.12.2023 17:00\n" +
+		"	Make smth2 02.12.2023 17:00\n" +
+		"	...\n" +
+		"/remind - reminds you about your doing, use this command like sort command\n" +
+		"/author - gets information about authors\n" + //implemented
+		"/delete - deletes doing from remind list,use this command like sort command\n" +
+		"/list - gets all doing from remind list" // correct english grammar
+	//"/change" //Add it later
+}
+
+func InfoRespond(botMessage *types.BotMessage) {
+	botMessage.Text = "This bot sorts your doing by Eisenhower's Matrix.\n" + "Eisenhower's Matrix is the one of the most popular sorting methods of doing.The essence of the technique is to sort tasks by importance and urgency using a special table"
+}
+
+func AuthorRespond(botMessage *types.BotMessage) {
+	botMessage.Text = "Ansar Rakhmimov. support: https://t.me/Rakhimov_Ans"
+}
+
+func ErrorRespond(botMessage *types.BotMessage) {
+	botMessage.Text = "unrecognized command, use /help to get list of commands"
 }
