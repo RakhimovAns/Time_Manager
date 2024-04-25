c0bdef511e13e3b1e97bba2ccff79a4838c06b78
Ansar <ansarrakhimov91@gmail.com>
2024-04-25 21:00:49 +0300

    edited inserts.sql

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -1,3 +1,11 @@
+CREATE TABLE doings(
+    id bigserial primary key ,
+    chat_id bigint,
+    name text,
+    importance integer,
+    time timestamp,
+    status boolean
+);
 CREATE TABLE languages(
     chat_id bigint,
     language text
